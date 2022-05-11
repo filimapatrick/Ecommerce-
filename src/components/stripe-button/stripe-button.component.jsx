2909de1190077e53c065525f5aa75fcdd2b823d2
filima patrick <filimapatrick30@gmail.com>
2022-05-11 20:01:46 +0100

    updates on package.json

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -3,7 +3,7 @@ import StripeCheckout from 'react-stripe-checkout';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
-  const publishableKey = 'pk_test_WBqax2FWVzS9QlpJScO07iuL';
+  const publishableKey = 'pk_test_51KwRFhAyTqZcGwrspfQIpdVVRd1rFWYVhWG14zzUyJZwNaLQJhwFjahyNPJoijt2rgfJ9D7o9oAYMTwTWYydsjO9008JDeYlli';
 
   const onToken = token => {
     console.log(token);
