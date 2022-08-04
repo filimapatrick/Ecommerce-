c44cfa7ec82ea85d8c84b2ce117ebf9a85b96294
filima patrick <filimapatrick30@gmail.com>
2022-08-04 23:44:36 +0100

    updates

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -3,10 +3,12 @@ import React from 'react';
 import Directory from '../../components/directory/directory.component';
 
 import '../../pages/homepage/homepage.styles.scss'
+import HomeProduct from './HomeProduct';
 
 const HomePage = () => (
   <div className='homepage'>
     <Directory />
+   <HomeProduct/>
   </div>
 );
 
